0c65c79521a7b362886d952a80a9a8fdf9e9f55b
Bogdan Fusa <fbm_95@yahoo.com>
2021-08-13 14:29:21 +0300

    added missing variable

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -2,5 +2,6 @@ module "dev_network" {
   source            = "../../modules/network"
   project_id        = var.project_id
   region            = var.region
+  env_name          = var.env_name
   subnet_cidr_range = "10.1.0.0/24"
 }
