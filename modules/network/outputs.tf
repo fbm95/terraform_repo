4230a109438ebcd7b3805d09bab8875eceda0164
Bogdan Fusa <fbm_95@yahoo.com>
2021-08-15 20:40:34 +0300

    refatoring

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,3 +1,10 @@
 output "network_name" {
   value = google_compute_network.vpc_network.name
 }
+
+output "subnetworks" {
+  value = {
+    "us-central1" = google_compute_subnetwork.subnetwork["us-central1"]
+    "europe-west1" = google_compute_subnetwork.subnetwork["europe-west1"]
+  }
+}
